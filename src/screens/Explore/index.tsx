66eded561bd42c4765a52710e75d2c71f1331a18
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-14 20:09:18 -0300

    Adiciona configurações iniciais para o mapa

--- src/screens/Explore/index.tsx
+++ src/screens/Explore/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { Title, Container } from './styles';
+import MapView from 'react-native-maps';
 
 const Explore = () => {
   return (
-    <Container>
-      <Title>Explorar</Title>
-    </Container>
+    <>
+      <Container>
+        <Title>Explorar</Title>
+      </Container>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </>
   );
 };
 
 export default Explore;
+
+const styles = StyleSheet.create({
+  map: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+});
