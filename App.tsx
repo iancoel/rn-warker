8488e9e1e8db5bf11f405c645ddb8989f12f5c0c
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-14 20:08:47 -0300

    Corrige paddingTop geral para dispositivos Android

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 import Explore from './src/screens/Explore';
 
 export default function App() {
@@ -13,5 +13,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F7F7F7',
     flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
