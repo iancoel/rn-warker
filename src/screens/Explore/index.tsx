40c2456fe6c417249fd0c90f2e67d2ac51f446f3
Ian Coelho <ian.coelho@meliuz.com.br>
2022-01-15 18:21:57 -0300

    Adiciona Card com estilos in-line  a screen Explore

--- src/screens/Explore/index.tsx
+++ src/screens/Explore/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Title, Container } from './styles';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { Title, Container, CardContainer, CardHeader } from './styles';
 import MapView from 'react-native-maps';
-import { Searchbar } from 'material-bread';
+import { Icon, Searchbar, Card, CardMedia } from 'material-bread';
+import cardBg from '../../assets/images/background-header-bg.png';
+import avatar from '../../assets/images/avatar.png';
 
 const Explore = () => {
   return (
@@ -35,6 +37,107 @@ const Explore = () => {
           navigationIcon="search"
           closeIcon="filter-list"
         />
+      </View>
+
+      <View
+        style={{
+          width: '100%',
+          alignItems: 'center',
+          position: 'absolute',
+          bottom: 115,
+        }}
+      >
+        <Card style={{ width: 334, height: 194 }}>
+          {/* Background */}
+          <CardMedia
+            image={
+              <Image
+                source={cardBg}
+                style={{
+                  width: '100%',
+                  height: 120,
+                  position: 'absolute',
+                  top: 0,
+                  resizeMode: 'cover',
+                  borderTopLeftRadius: 10,
+                  borderTopRightRadius: 10,
+                }}
+              />
+            }
+          />
+
+          {/* Barra de avaliações e favoritos */}
+          <View
+            style={{
+              width: '100%',
+              paddingLeft: 20,
+              paddingRight: 12,
+              paddingTop: 10,
+              position: 'absolute',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Icon name="star" size={24} color={'#FFB600'} />
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: 22,
+                  color: '#FFFFFF',
+                  marginLeft: 5,
+                }}
+              >
+                5.0 (70+)
+              </Text>
+            </View>
+            <Icon name="favorite" size={24} color={'#E12114'} />
+          </View>
+
+          {/* Avatar e título */}
+          <View
+            style={{
+              width: '100%',
+              position: 'absolute',
+              top: 83,
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+          >
+            <Image
+              source={avatar}
+              style={{
+                width: 74,
+                height: 74,
+                borderRadius: 37,
+                borderColor: '#FDFDFD',
+                borderWidth: 2,
+              }}
+            />
+
+            <View style={{ marginLeft: 10 }}>
+              <Text
+                style={{
+                  color: '#F7F7F7',
+                  fontWeight: 'bold',
+                  fontSize: 24,
+                  marginBottom: 15,
+                }}
+              >
+                Posto do Wark
+              </Text>
+              <Text
+                style={{ color: '#65696B', fontSize: 14, marginBottom: 10 }}
+              >
+                Salvamos você com gasolina
+              </Text>
+              <Text style={{ color: '#9AA0A1', fontSize: 12 }}>
+                Garanta sua espada por R$ 9.99!
+              </Text>
+            </View>
+          </View>
+        </Card>
       </View>
     </>
   );
--- src/screens/Explore/styles.ts
+++ src/screens/Explore/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import cardHeaderBg from '../../assets/images/background-header-bg.png';
 
 export const Container = styled.View`
   height: 60px;
@@ -11,4 +12,11 @@ export const Container = styled.View`
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
+`;
+
+export const CardContainer = styled.View`
+  height: 194px;
+  width: 334px;
+  background: yellow;
+  border-radius: 10px;
 `;
